897d01b091d85cee8fb38d571fdca093ff17e110
rrramko <rrramko@gmail.com>
2015-03-29 17:01:34 +0300

    1
    1

---
+++ www/action.php
@@ -0,0 +1,19 @@
+﻿<?php 
+
+include ('login.php');
+$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+if (!$db_server) die("Неможливо підключитись до MySQL: " . mysql_error());
+ $firstname = $_POST['firstname']; 
+ $lastname = $_POST['lastname']; 
+ $predmet = $_POST['comment'];
+ $pbat = $_POST['pbat'];
+ $sql = 'INSERT INTO diplom.users(firstname, lastname, predmet, pbat)
+ VALUES("'.$firstname.'", "'.$lastname.'", "'.$predmet.'", "'.$pbat.'")';
+//mysql_query($sql) or die(mysql_error());
+if(!mysql_query($sql))
+ {echo '<center><p style="color:red;"><b>Помилка додавання даних!</b></p></center>';}
+ else
+ {echo '<center><p style="color:green;"><b>Дані успішно додані!</b></p></center>';}
+ echo "<center><a href='/'>Назад</a></center>";
+?>
+
---
+++ www/index.php
@@ -0,0 +1,39 @@
+<html>
+<?php
+require_once 'login.php';
+$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+if (!$db_server) die("Неможливо підключитись до MySQL: " . mysql_error());
+?>
+<form name="test" method="post" action="action.php">
+    
+  <p><b>Прізвище:</b><Br>
+   <input type="text" name="lastname" size="40">
+  </p>
+  <p><b>Ім’я:</b><br>
+   <input type="text" name="firstname" size="40">
+  </p>
+     <p><b>По батькові:</b><Br>
+   <input type="text" name="pbat" size="40">
+  </p>
+  <p>Предмети які викладає:<Br>
+   <textarea name="comment" cols="40" rows="3"></textarea></p>
+   <p><input type="checkbox" name="a" value="pr1"> Предмет 1</p>
+   <p><input type="checkbox" name="a" value="pr2"> Предмет 2</p>
+   <p><input type="checkbox" name="a" value="pr3"> Предмет 3</p>
+   <p><input type="checkbox" name="a" value="pr4"> Предмет 4</p>
+   <p><input type="checkbox" name="a" value="pr5"> Предмет 5</p>
+   <p><select size="1" name="stupin" required>
+    <option disabled selected value>Вчений ступінь</option>
+    <option value="k_n">Кандидат наук</option>
+    <option value="d_n">Доктор наук</option>
+   </select> 
+   <select size="1" name="posada" required>
+    <option disabled selected value>Посада</option>
+    <option value="ac">Асистент</option>
+    <option value="vk">Викладач</option>
+	<option value="dc">Доцент</option>
+	<option value="pr">Професор</option>
+   </select></p>
+     <p><input type="submit" value="ОК"> <input type="reset" value="Clear"></p>
+     </form>
+</html>
