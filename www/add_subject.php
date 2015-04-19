7f7c8d0e134bcf84e09e0c3560dec01d7bed104f
rrramko <rrramko@gmail.com>
2015-04-19 22:33:44 +0300

    Редагнув

--- www/add_subject.php
+++ www/add_subject.php
@@ -3,26 +3,17 @@
   <p><b>Предмет:</b><br>
    <input type="text" name="subject_name" size="40">
   </p>
- <p><b>Загальна кылькість годин:</b><br>
+ <p><b>Загальна кількість годин:</b><br>
    <input type="text" name="all_hours" size="40">
   </p>
   <p><b>Годин індивідуальної роботи:</b><br>
    <input type="text" name="ind_hours" size="40">
   </p>
-  <p><b>Лекцій:</b><br>
+  <p><b>Лекційних годин:</b><br>
    <input type="text" name="l_hours" size="40">
   </p>
-  <p><b>Практичних:</b><br>
+  <p><b>Практичних/лабораторних годин:</b><br>
    <input type="text" name="pr_hours" size="40">
-  </p>
-  <p><b>Семінарський:</b><br>
-   <input type="text" name="sem_hours" size="40">
-  </p>
-   <p><b>Лабораторних:</b><br>
-   <input type="text" name="lab_hours" size="40">
-  </p>
-   <p><b>Самостійної роботи студента:</b><br>
-   <input type="text" name="stud_hours" size="40">
   </p>
   <input type="hidden" name="active" value="1">
   <input type="hidden" name="action" value="add_subject">
