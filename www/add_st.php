e63ba0472c753145eedc9d2ec1aef4a9908bf5a7
see525 <capone1@mail.ua>
2015-04-20 00:27:26 +0300

    алгоритм

--- www/add_st.php
+++ www/add_st.php
@@ -17,57 +17,57 @@ while($res_t=mysql_fetch_array($query_t))
 }
 
  if (isset($_GET[id])) { 
+  
+ $query="SELECT * FROM teach_subj  where teacher_id=".(int)$_GET[id];     
+$query_st= mysql_query($query) or die ("Error:".mysql_error()); 
+ echo '<table border="1">
+     <tr> <td><b>Назва пердмету: </b></td><th>';
+	 echo 'Всього годин: </th><th> Індивідуальної<br>роботи: </th><th>Лекційних<br>занять:</th><th>Практичних/<br>Лабораторних<br>занять:</th></tr>';
+   
+while($res_ts=mysql_fetch_array($query_st)) 
+{  
+   $l_hours_i=$l_hours_i+$res_ts['l_hours'];
+   $pr_hours_i=$pr_hours_i+$res_ts['pr_hours'];
+   $ind_hours_i=$ind_hours_i+$res_ts['ind_hours'];   
+   echo '<tr><td>'.$res_ts['subject'].'</td><td align="right">0</td><td align="right">'.$res_ts['ind_hours'].'</td><td align="right">'.$res_ts['l_hours'].'</td><td align="right">'.$res_ts['pr_hours'].'</td></tr>';
+ 
+}
+  echo '<tr><td><b>Разом: </b><br></td><td align="right"></td><td align="right">'.$ind_hours_i.'</td><td align="right">'.$l_hours_i.'</td><td align="right">'.$pr_hours_i.'</td></tr></table><br><br>';
+
+
+
+ 
+ //оригінал даних з годинами нормально можна вивести
  $query="SELECT * FROM teacher  where id=".(int)$_GET[id];    
 $query_id= mysql_query($query) or die ("Error:".mysql_error()); 
 $tea=mysql_fetch_assoc($query_id);
  echo 'Ви вибрали викладача: '.$tea['teacher_name'].'<br><br>';
- 
- echo '<br>Він викладає такі предмети:<br>';
+ echo 'В нього стільки годин вільних: '.$tea['hour'].'<br><br>';
+ echo '<br>Оригінальна кількість годин в предмета:<br>';
   $query="SELECT * FROM teach_subj  where teacher_id=".(int)$_GET[id];    
 $sub= mysql_query($query) or die ("Error:".mysql_error());
-     ?>  <table border="1">
-     <tr> <td><?echo '<b>Назва пердмету: </b>'?></td><th> <?echo 'Всього годин: ';?></th><th><? echo 'Індивідуальної'?><br><? echo ' роботи: ';?></th><th> <?  echo 'Лекційних'?><br><? echo ' занять: ';?></th><th> <?  echo 'Практичних/'?><br><? echo 'Лабораторних'?><br><? echo ' занять: ';?></th></tr><?
+       echo '<table border="1">
+     <tr> <td><b>Назва пердмету: </b></td><th>';
+	 echo 'Всього годин: </th><th> Індивідуальної<br>роботи: </th><th>Лекційних<br>занять:</th><th>Практичних/<br>Лабораторних<br>занять:</th></tr>';
      while($subjects=mysql_fetch_array($sub))
 { 
-$query="SELECT * FROM subject  where subject_name='".$subjects['subject']."'";    
+$query="SELECT * FROM subject_original  where subject_name='".$subjects['subject']."'";    
 $sub_name=mysql_query($query) or die ("Error:".mysql_error()); 
 $su=mysql_fetch_assoc($sub_name);
 
 $l_hours=$l_hours+$su['l_hours'];
 $pr_hours=$pr_hours+$su['pr_hours'];
-$sem_hours=$sem_hours+$su['sem_hours'];
-$lab_hours=$lab_hours+$su['lab_hours'];
 $ind_hours=$ind_hours+$su['ind_hours'];
+echo '<tr><td>'.$su['subject_name'].'</td><td align="right">'.$su['all_hours'].'</td><td align="right">'.$su['ind_hours'].'</td><td align="right">'.$su['l_hours'].'</td><td align="right">'.$su['pr_hours'].'</td></tr>';
 
-   ?>
-    <tr><td><?echo ''.$su['subject_name'].'' ?></td><td align="right"><? echo ''.$su['all_hours'].''?></td><td align="right"><? echo ''.$su['ind_hours'].''?></td><td align="right"><? echo ''.$su['l_hours'].''?></td><td align="right"><? echo ''.$su['pr_hours'].''?></td></tr>
+}
 
-<?/*
-echo '<b>Назва пердмету: </b>'.$su['subject_name'].'; ';
-  echo '<b>Всього годин: </b>'.$su['all_hours'].'; ';
-   echo '<b>Індивідуально годин: </b>'.$su['ind_hours'].'; ';
-   echo '<b>Лекційних годин: </b>'.$su['l_hours'].'; ';
-   echo '<b>Практичних годин: </b>'.$su['pr_hours'].'; ';
-   echo '<b>Семінари годин: </b>'.$su['sem_hours'].'; ';
-   echo '<b>Лабалаторні годин: </b>'.$su['lab_hours'].'; ';
-   echo '<b>Самостійне вивч годин: </b>'.$su['stud_hours'].'; <br>';*/
-}?>
-
-     <tr><td><?echo '<b>Разом: </b><br>'?></td><td align="right"></td><td align="right"><? echo ''.$ind_hours.''?></td><td align="right"><? echo ''.$l_hours.''?></td><td align="right"><? echo ''.$pr_hours.''?></td></tr>
+     echo '<tr><td><b>Разом: </b><br></td><td align="right"></td><td align="right">'.$ind_hours.'</td><td align="right">'.$l_hours.'</td><td align="right">'.$pr_hours.'</td></tr></table>';
 
 
+   
 
- </table>
-<?/*
-   echo '<b>Разом: </b><br>';
-     echo '<b>Всіх годин: </b>'.$all_hours.'; ';
-   echo '<b>Лекційних годин: </b>'.$l_hours.'; ';
-   echo '<b>Практичних годин: </b>'.$pr_hours.'; ';
-   echo '<b>Семінари годин: </b>'.$sem_hours.'; ';
-   echo '<b>Лабалаторні годин: </b>'.$lab_hours.'; <br>';
-     echo '<b>Індивідуальної роботи: </b>'.$ind_hours.'; <br>';*/
-   $sum_hours=900-($l_hours+$pr_hours+$sem_hours+$lab_hours);
- echo '<b>У викладача залишилося </b>' .$sum_hours. '<b> год. </b>';
+ 
    //це файл можна розділити код що дальше йде відповідає за добавлення предмету
 
 echo '<h2>Добавити йому предмети</h2><br>';
@@ -75,20 +75,20 @@ echo '<h2>Добавити йому предмети</h2><br>';
 $query_sub="SELECT * FROM subject where active='1' LIMIT 0,50";    
 $query_s= mysql_query($query_sub) or die ("Error:".mysql_error()); 
 
-echo '<form name="st" method="post" action="/inc/action.php">';
-    ?><table border="1">
-   <tr> <td></td><th> <?echo 'Всього годин: ';?></th><th><? echo 'Індивідуальної'?><br><? echo ' роботи: ';?></th><th> <?  echo 'Лекційних'?><br><? echo ' занять: ';?></th><th> <?  echo 'Практичних/'?><br><? echo 'Лабораторних'?><br><? echo ' занять: ';?></th></tr><?
+echo '<form name="st" method="post" action="/inc/action.php">
+    <table border="1">
+   <tr> <td></td><th> Всього годин: </th><th>Індивідуальної<br>роботи: </th><th>Лекційних<br>занять: </th><th> Практичних/<br>Лабораторних<br>занять: </th></tr>';
 while($res_s=mysql_fetch_array($query_s)) 
-{?><tr><td><?   echo '<input name="subject[]" type="checkbox" value="'.$res_s['subject_name'].'">'.$res_s['subject_name'].'';?></td>
-   <td align="right"><? echo ''.$res_s['all_hours'].''?></td><td align="right"><? echo ''.$res_s['ind_hours'].''?></td><td align="right"><? echo ''.$res_s['l_hours'].''?></td><td align="right"><? echo ''.$res_s['pr_hours'].''?></td></tr>
- <?}?></table>
-    <!--  <td> <? echo 'Індивідуально годин: ';?></td><tr><td></td><td><? echo ''.$res_s['ind_hours'].'; ';?></td></tr>
-        <td> <?  echo 'Лекційних годин: ';?></td><td><? echo ''.$res_s['l_hours'].'; ';?></td>
-       <td> <?  echo 'Практичних годин: ';?></td><td><? echo ''.$res_s['pr_hours'].'; ';?></td>
-        <td> <?  echo 'Семінари годин: ';?></td><td><? echo ''.$res_s['sem_hours'].'; ';?></td>
-        <td> <?  echo 'Лабалаторні годин: ';?></td><td><? echo ''.$res_s['lab_hours'].'; ';?></td>
-       <td> <?  echo 'Самостійне вивч годин: ';?></td><td><? echo ''.$res_s['stud_hours'].'; <br>';?></td></tr>-->
-<?
+{
+	echo '<tr><td><input name="subject[]" type="checkbox" value="'.$res_s['subject_name'].'">'.$res_s['subject_name'].'</td>
+   <td align="right">'.$res_s['all_hours'].'</td><td align="right">'.$res_s['ind_hours'].'</td><td align="right">'.$res_s['l_hours'].'</td><td align="right">'.$res_s['pr_hours'].'</td></tr>';
+ }echo '</table>
+    <!--  <td> Індивідуально годин: </td><tr><td></td><td>'.$res_s['ind_hours'].'</td></tr>
+        <td> Лекційних годин:</td><td>'.$res_s['l_hours'].'</td>
+       <td> Практичних годин: </td><td>'.$res_s['pr_hours'].'</td>
+        <td> Семінари годин: </td><td>'.$res_s['sem_hours'].'</td>
+        <td> Лабалаторні годин:</td><td>'.$res_s['lab_hours'].'</td>
+       <td> Самостійне вивч годин:</td><td>'.$res_s['stud_hours'].' <br></td></tr>-->';
 echo '<input type="hidden" name="teacher_id" value="'.(int)$_GET[id].'"> ';
 echo '<input type="hidden" name="action" value="add_subj_to_teach"> ';
 echo '<input type="submit" value="ОК"> ';
--- www/inc/function.php
+++ www/inc/function.php
@@ -5,6 +5,8 @@
  */
 function add_teacher($teacher_name,$hour)
 {
+	 $teacher_name=mysql_real_escape_string(htmlspecialchars($teacher_name));
+	 $hour=(int)$hour;
   $sql = 'INSERT INTO teacher(teacher_name,hour)
  VALUES("'.$teacher_name.'", "'.$hour.'")';
  if(!mysql_query($sql))
@@ -12,6 +14,7 @@ function add_teacher($teacher_name,$hour)
  else
  {echo '<center><p style="color:green;"><b>Данные добавлены!</b></p></center>';}
 }
+
 
 
 /**
@@ -25,10 +28,19 @@ function add_teacher($teacher_name,$hour)
  * @param $stud_hours
  * @param $active
  */
-function add_subject($subject_name,$all_hours,$ind_hours,$l_hours,$pr_hours,$sem_hours,$lab_hours,$stud_hours,$active)
+function add_subject($subject_name,$all_hours,$ind_hours,$l_hours,$pr_hours,$active)
 {
-  $sql = 'INSERT INTO subject(subject_name,all_hours,ind_hours,l_hours,pr_hours,sem_hours,lab_hours,stud_hours,active)
- VALUES("'.$subject_name.'", "'.$all_hours.'", "'.$ind_hours.'", "'.$l_hours.'", "'.$pr_hours.'", "'.$sem_hours.'", "'.$lab_hours.'", "'.$stud_hours.'", "'.$active.'")';
+	$subject_name=mysql_real_escape_string(htmlspecialchars($subject_name));
+	$all_hours=(int)$all_hours;
+	$ind_hours=(int)$ind_hours;
+	$l_hours=(int)$l_hours;
+	$pr_hours=(int)$pr_hours;
+	$active=(int)$active;
+  $sql = 'INSERT INTO subject(subject_name,all_hours,ind_hours,l_hours,pr_hours,active)
+ VALUES("'.$subject_name.'", "'.$all_hours.'", "'.$ind_hours.'", "'.$l_hours.'", "'.$pr_hours.'","'.$active.'")';
+ $sql2 = 'INSERT INTO subject_original(subject_name,all_hours,ind_hours,l_hours,pr_hours)
+ VALUES("'.$subject_name.'", "'.$all_hours.'", "'.$ind_hours.'", "'.$l_hours.'", "'.$pr_hours.'")';
+ mysql_query($sql2) or die ("Error:".mysql_error());
  if(!mysql_query($sql))
  {echo mysql_error().'<center><p style="color:red;"><b>Ошибка при добавлении данных!</b></p></center>';}
  else
@@ -41,7 +53,15 @@ function add_subject($subject_name,$all_hours,$ind_hours,$l_hours,$pr_hours,$sem
  */
 function add_subj_to_teach($teacher_id,$subject)
 {
-	if(empty($subject))
+	$teacher_id=mysql_real_escape_string(htmlspecialchars($teacher_id));
+	$subject=(int)$subject;
+//взнаємо кількість годин у викладача	
+$query="SELECT * FROM teacher  where id=".$teacher_id;    
+$q_hour= mysql_query($query) or die ("Error:".mysql_error()); 
+$hour_array=mysql_fetch_assoc($q_hour);
+$hour=$hour_array['hour'];
+ 
+if(empty($subject))
   {
     echo("Ви не вибрали предмет");
   }
@@ -50,14 +70,76 @@ function add_subj_to_teach($teacher_id,$subject)
     $N = count($subject);
     for($i=0; $i < $N; $i++)
     {
-     $sql = 'INSERT INTO teach_subj(teacher_id,subject)
-     VALUES("'.$teacher_id.'", "'.$subject[$i].'")';
-     $sql2 = 'UPDATE  subject SET  active="0" WHERE  subject.subject_name ="'.$subject[$i].'"';
-      $result = mysql_query($sql2) or die ("Error: ".mysql_error());
+	 //рахуємо вибрані години
+	 $query="SELECT * FROM subject  where subject_name='".$subject[$i]."'";    
+     $sub_name=mysql_query($query) or die ("Error:".mysql_error()); 
+     $su=mysql_fetch_assoc($sub_name);
+	 if ($su['l_hours']<$hour&&$hour>0) {
+		 $l_hours=$su['l_hours'];
+		 $hour=$hour-$su['l_hours'];
+		 $active=0;
+		 mysql_query('UPDATE  teacher SET  hour="'.$hour.'" WHERE  teacher.id ="'.$teacher_id.'"') or die ("Error: ".mysql_error());
+	     mysql_query('UPDATE  subject SET  l_hours="0" WHERE  subject.subject_name ="'.$subject[$i].'"') or die ("Error: ".mysql_error());
+     }
+	 else 
+	 {
+		$reshta=$su['l_hours']-$hour;
+		$l_hours=$hour;
+		$hour=0;
+		$active=1;
+		mysql_query('UPDATE  teacher SET  hour="0" WHERE  teacher.id ="'.$teacher_id.'"') or die ("Error: ".mysql_error());
+		mysql_query('UPDATE  subject SET  l_hours="'.$reshta.'" WHERE  subject.subject_name ="'.$subject[$i].'"') or die ("Error: ".mysql_error());
+	 }
+	 if ($su['pr_hours']<$hour&&$hour>0) {
+		 $pr_hours=$su['pr_hours'];
+		 $hour=$hour-$su['pr_hours'];
+		 $active=0;
+		 mysql_query('UPDATE  teacher SET  hour="'.$hour.'" WHERE  teacher.id ="'.$teacher_id.'"') or die ("Error: ".mysql_error());
+	     mysql_query('UPDATE  subject SET  pr_hours="0" WHERE  subject.subject_name ="'.$subject[$i].'"') or die ("Error: ".mysql_error());
+
+	 }
+	 else 
+	 {
+		$reshta=$su['pr_hours']-$hour;
+		$pr_hours=$hour;
+		$hour=0;
+		$active=1;
+		mysql_query('UPDATE  teacher SET  hour="0" WHERE  teacher.id ="'.$teacher_id.'"') or die ("Error: ".mysql_error());
+		mysql_query('UPDATE  subject SET  pr_hours="'.$reshta.'" WHERE  subject.subject_name ="'.$subject[$i].'"') or die ("Error: ".mysql_error());
+	 }
+	 if ($su['ind_hours']<$hour&&$hour>0) {
+		 $ind_hours=$su['ind_hours'];
+		 $hour=$hour-$su['ind_hours'];
+		 $active=0;
+		 mysql_query('UPDATE  teacher SET  hour="'.$hour.'" WHERE  teacher.id ="'.$teacher_id.'"') or die ("Error: ".mysql_error());
+	     mysql_query('UPDATE  subject SET  ind_hours="0" WHERE  subject.subject_name ="'.$subject[$i].'"') or die ("Error: ".mysql_error());
+
+	 }
+	 else 
+	 {
+		$reshta=$su['ind_hours']-$hour;
+		$ind_hours=$hour;
+		$hour=0;
+		$active=1;
+		mysql_query('UPDATE  teacher SET  hour="0" WHERE  teacher.id ="'.$teacher_id.'"') or die ("Error: ".mysql_error());
+		mysql_query('UPDATE  subject SET  ind_hours="'.$reshta.'" WHERE  subject.subject_name ="'.$subject[$i].'"') or die ("Error: ".mysql_error());
+	 }
+	 
+    // $l_hours=$l_hours+$su['l_hours'];
+    // $pr_hours=$pr_hours+$su['pr_hours'];
+   //  $ind_hours=$ind_hours+$su['ind_hours'];
+	 //$res_hour=
+      $sql = 'INSERT INTO teach_subj(teacher_id,subject,ind_hours,l_hours,pr_hours)
+      VALUES("'.$teacher_id.'", "'.$subject[$i].'", "'.$ind_hours.'", "'.$l_hours.'", "'.$pr_hours.'")';
      if(!mysql_query($sql))
      {echo mysql_error().'<center><p style="color:red;"><b>Ошибка при добавлении данных!</b></p></center>';}
       else
      {echo '<center><p style="color:green;"><b>Данные добавлены!</b></p></center>';}
+     $sql2 = 'UPDATE  subject SET  active="'.$active.'" WHERE  subject.subject_name ="'.$subject[$i].'"';
+     mysql_query($sql2) or die ("Error: ".mysql_error());
+	 
+	    
+    
       
     }
   }  
